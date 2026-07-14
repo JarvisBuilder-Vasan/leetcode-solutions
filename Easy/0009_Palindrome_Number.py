5b082931cd8927ee30e980f85ab4b3499f2e1f86
JarvisBuilder-Vasan <bssrinivasanofficial@gmail.com>
2026-07-14 12:16:46 +0530

    Add solution for LeetCode #9 - Palindrome Number

---
+++ Easy/0009_Palindrome_Number.py
@@ -0,0 +1,16 @@
+# LeetCode 9 - Palindrome Number
+# Difficulty: Easy
+# Approach: Reverse Number
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        n=x
+        r=0
+        while(x>0):
+            d=x%10
+            r=r*10+d
+            x=x//10
+
+        return n==r
