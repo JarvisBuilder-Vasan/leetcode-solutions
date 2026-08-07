f5d93476a0588d485668a73a582a39ab58030176
Srinivasan B S <bssrinivasanofficial@gmail.com>
2026-08-07 22:44:37 +0530

    Add LeetCode #217 - Contains Duplicate

---
+++ Easy/HashMap/0217_Contains_Duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic={}
+
+        for num in nums:
+            if num in dic:
+                dic[num]+=1
+            else:
+                dic[num]=1
+            
+        for num in nums:
+            if dic[num]>1:
+                return True
+        return False
