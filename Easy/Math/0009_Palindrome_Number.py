355cb6b9f4c163958604a5a9f73200b654baa9d1
Srinivasan B S <bssrinivasanofficial@gmail.com>
2026-08-10 22:39:17 +0530

    Add 0009_Palindrome_Number.py

---
+++ Easy/Math/0009_Palindrome_Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        n=x
+        r=0
+        while(x>0):
+            d=x%10
+            r=r*10+d
+            x=x//10
+
+        return n==r
