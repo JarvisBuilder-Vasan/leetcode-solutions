13679bbc9be128c7e4efda83fb15f6752aae2fbf
Srinivasan B S <bssrinivasanofficial@gmail.com>
2026-09-17 21:52:03 +0530

    Add 0540_Single_Element_in_a_Sorted_Array.py

---
+++ Easy/Binary_Search/0540_Single_Element_in_a_Sorted_Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        result=0
+        for i in range(len(nums)):
+            result^=nums[i]
+
+        return result
